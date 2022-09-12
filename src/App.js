c72e2464eb91ceb07da930513140e4518e7cf565
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-09-12 17:47:27 +0800

    feat: countdown bryanzacarias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 
+
 function App() {
+  const [seconds, setSeconds] = useState(10);
+
+  useEffect(() => {
+    seconds > 0 ? setTimeout(() => setSeconds(seconds - 1), 1000) : setSeconds(0);
+  }, [seconds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+        <h1>Count down from 10: {seconds}</h1>
+        <pre>The way to get started is to quit talking and begin doing...</pre>
+    </header>
+  </div>
   );
 }
 
